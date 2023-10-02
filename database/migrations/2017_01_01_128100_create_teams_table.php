ad1f30fc261f87a80652edbf6ccd19c3e2577056
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2023-10-02 23:14:04 +0000

    resolve foreign key reference

--- database/migrations/2017_01_01_128100_create_teams_table.php
+++ database/migrations/2017_01_01_128100_create_teams_table.php
@@ -9,7 +9,7 @@ class CreateTeamsTable extends Migration
     public function up()
     {
         Schema::create('teams', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
 
             $table->string('name')->unique()->index();
 
--- database/migrations/2017_01_01_128200_create_team_user_pivot_table.php
+++ database/migrations/2017_01_01_128200_create_team_user_pivot_table.php
@@ -9,13 +9,9 @@ class CreateTeamUserPivotTable extends Migration
     public function up()
     {
         Schema::create('team_user', function (Blueprint $table) {
-            $table->integer('team_id')->unsigned()->index();
-            $table->foreign('team_id')->references('id')->on('teams')
-                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('team_id')->constrained()->index()->onUpdate('cascade')->onDelete('cascade');
 
-            $table->integer('user_id')->unsigned()->index();
-            $table->foreign('user_id')->references('id')->on('users')
-                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->index()->onUpdate('cascade')->onDelete('cascade');
 
             $table->primary(['user_id', 'team_id']);
 
